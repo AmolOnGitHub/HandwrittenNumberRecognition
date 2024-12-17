4729f2a952c106409596856b0a38093ad81d1a20
Amol Vijayachandran <63136382+AmolOnGitHub@users.noreply.github.com>
2024-12-17 21:43:02 +0530

    changed demonstrate

--- demonstrate.py
+++ demonstrate.py
@@ -4,20 +4,19 @@ import load_mnist
 import matplotlib.pyplot as plt
 import numpy as np
 from PIL import Image
+import random
+
+image = "data/images/test.png"
+model = 'results/93_784-16-32-16-10/'
 
 
-image = "data/images/a.png"
-model = 'results/87_784-16-10/'
+images, labels = load_mnist.Data('data/train-images.idx3-ubyte', 'data/train-labels.idx1-ubyte', 'data/t10k-images.idx3-ubyte', 'data/t10k-labels.idx1-ubyte').get_test_data()
 
-
-## Get image as vector and normalise
-
-img = Image.open(image).convert('L')
-img = np.array(img).flatten().reshape(784, 1) / 255
-
+index = random.randint(0, len(images) - 1)
+img = images[index].flatten().reshape(784, 1) / 255
 
 plt.imshow(img.reshape(28, 28), cmap='gray')
-plt.title('Input Image')
+plt.title(f'Image: {labels[index]}')
 plt.show()
 
 
